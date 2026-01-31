3f6d448599828b8e2905107a598448169479d329
msanjai2005 <msanjai1012@gmail.com>
2026-02-01 00:34:53 +0530

    updated

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:3000/api",
-  withCredentials: true, // 🔑 THIS IS THE CORE CHANGE
+  baseURL: "https://global-trend-task.onrender.com/api",
+  withCredentials: true,
 });
 
 export default API;
